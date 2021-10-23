b9076000e1262844ac1be20fc550dd1b141c59a0
will <was51@pitt.edu>
2021-10-23 01:40:36 -0400

    Added Navbar

--- site.py
+++ site.py
@@ -1,10 +1,23 @@
+import re
 from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name)
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/schedule")
+def schedule():
+    return render_template("schedule.html")
+
+@app.route("/gen-ed")
+def gen_ed():
+    return render_template("gen_ed.html")
+
+@app.route("/major-requirements")
+def major():
+    return render_template("major.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
